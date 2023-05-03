28624836e219995c33f5d4aa42ee30f535fe2564
Andrew <82255762+AndrewBalin@users.noreply.github.com>
2023-05-03 12:24:00 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from data import db_session
+
 from flask import Flask
 #a
 app = Flask(__name__)
@@ -7,10 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def mission():
     return "EBASH"
-
-def main():
-    db_session.global_init("db/main_db.db")
-    app.run()
 
 
 if __name__ == '__main__':
